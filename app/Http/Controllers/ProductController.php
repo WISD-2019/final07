42ff2e7a79e2dc26afd96e93584ebf0880130aec
3A632014 <vul3545677@gmail.com>
2020-01-06 06:32:26 +0800

    新增購物商城的路由設定

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -47,6 +48,7 @@ class ProductController extends Controller
     public function show(Product $product)
     {
         //
+
     }
 
     /**
@@ -81,5 +83,64 @@ class ProductController extends Controller
     public function destroy(Product $product)
     {
         //
+    }
+
+    //購物商城
+    public function shop()
+    {
+        $data = Product::all();
+        return view('shop',['products' => $data]);
+    }
+
+    //價格排序
+    public function asc()
+    {
+        $data = Product::orderBy('asc')->get();
+        return view('shop',['products' => $data]);
+    }
+    public function desc()
+    {
+        $data = Product::orderBy('desc')->get();
+        return view('shop',['products' => $data]);
+    }
+    public function price($type)
+    {
+        $data = Product::orderBy('price',$type)->get();
+        return view('shop',['products' => $data]);
+    }
+
+    //搜尋
+    public function search(Request $request)
+    {
+        $search = $request->input('search');
+        $show = Product::where('productname','like','%'.$search.'%')->all();
+        return view('shop',['products'=>$show,'search'=>$search]);
+    }
+
+    //衣服分類
+    public function clothe()
+    {
+        $data = DB::table('Products')
+            ->where('products.type','=','clothe')
+            ->get();
+        return view('shop',['products'=>$data]);
+    }
+
+    //褲子分類
+    public function pants()
+    {
+        $data = DB::table('Products')
+            ->where('products.type','=','pants')
+            ->get();
+        return view('shop',['products'=>$data]);
+    }
+
+    //外套分類
+    public function coat()
+    {
+        $data = DB::table('Products')
+            ->where('products.type','=','coat')
+            ->get();
+        return view('shop',['products'=>$data]);
     }
 }
